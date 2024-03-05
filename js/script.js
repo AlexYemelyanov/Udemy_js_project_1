05dd54b9192e50a1dde510c99ad3e458f3fc3f16
Alexey <a.yemelyanovfe@gmail.com>
2024-03-05 11:55:40 +0300

    hw_1: end

--- js/script.js
+++ js/script.js
@@ -14,13 +14,51 @@
 
 'use strict';
 
+const adv = document.querySelectorAll('.promo__adv img');
+const genre = document.querySelector('.promo__genre');
+const bgr = document.querySelector('.promo__bg');
+const movieList = document.querySelector('.promo__interactive-list');
+
+adv.forEach((item) => {
+	item.remove();
+});
+
+genre.textContent = 'драма';
+console.log(bgr);
+bgr.style.background = `url(../img/bg.jpg)`;
+
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+	movies: [
+		'Логан',
+		'Лига справедливости',
+		'Ла-ла лэнд',
+		'Одержимость',
+		'Скотт Пилигрим против...',
+	],
+};
+const movieInsert = (obj) => {
+	let i = 1;
+	movieList.innerHTML = '';
+	obj.movies.sort();
+	obj.movies.forEach((item) => {
+		movieList.innerHTML += `
+        <li class="promo__interactive-item">${i}. ${item}
+            <div class="delete"></div>
+        </li>
+    `;
+		i++;
+	});
+	console.log(obj.movies.length);
+	// 	for (let i = 1; i <= movieDB.movies.length; i++) {
+
+	// 	}
+	//
 };
 
+movieInsert(movieDB);
+
+// movieList.innerHTML = `
+//     <li class="promo__interactive-item">${item}
+//         <div class="delete"></div>
+//     </li>
+// `;
